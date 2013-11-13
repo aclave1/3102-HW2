8436ccaf46b071693ff6944c84302ec5d31a77d0
alex clavelle <aclav3113@gmail.com>
2013-11-13 00:53:56 -0600

    Made some serious headway with the linked list implementation.

--- Trie/ListTrie.java
+++ Trie/ListTrie.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class ListTrie {
 	Node root;
-	
+
 	public class Node {
 		char lead;
 		String label;
@@ -18,48 +18,81 @@ public class ListTrie {
 			label = s;
 			children = new LinkedList<Node>();
 		}
-		public Node(char l){
-			children = new LinkedList<Node>();
+
+		public Node(char l) {
 			lead = l;
+			children = new LinkedList<Node>();
+		}
+
+		@Override
+		public String toString() {
+
+			return new String("" + lead + ": " + label);
+
 		}
 	}
-	
+
 	public ListTrie() {
 		root = new Node();
 		root.children = new LinkedList<Node>();
 	}
+
 	/**
 	 * 
-	 * @param key : The character we're looking for
-	 * @param node the node whose children we're searching
-	 * @return null if the node does not have the child, a reference to the child if it does. 
+	 * @param key
+	 *            : The character we're looking for
+	 * @param node
+	 *            the node whose children we're searching
+	 * @return null if the node does not have the child, a reference to the
+	 *         child if it does.
 	 */
-	public Node lSearch(char key,Node node){
-		for(Node child : node.children){
-			if(node.lead == key){
+	public Node lSearch(char key, Node node) {
+		for (Node child : node.children) {
+			if (child.lead == key) {
 				return child;
 			}
 		}
-		
 		return null;
 	}
-	public void insertString(String s){
+
+	public boolean tSearch(String s) {
 		char[] chars = s.toCharArray();
 		Node n = root;
-		for(int i=0;i<chars.length;i++){
-			if(this.lSearch(chars[i],n) == null){
+		for (int i = 0; i < chars.length; i++) {
+			if (this.lSearch(chars[i], n) == null) {
+				return false;
+			} else {
+				if (i == chars.length) {
+					return true;
+				}
+				n = this.lSearch(chars[i], n);
+			}
+		}
+		return false;
+	}
+
+	public void insertString(String s) {
+		char[] chars = s.toCharArray();
+		Node n = root;
+		for (int i = 0; i < chars.length; i++) {
+			if (this.lSearch(chars[i], n) == null) {
 				this.insertChar(chars[i], n);
 				n = this.lSearch(chars[i++], n);
+			} else {
+				n = this.lSearch(chars[i++], n);
 			}
-			n=this.lSearch(chars[i++], n);
 		}
 	}
-	
-	public void insertChar(char c, Node n){
-		if(this.lSearch(c,n) == null){
-			n.children.add(new Node(c));
+
+	public void insertChar(char c, Node n) {
+
+		for (Node child : n.children) {
+			if (child.lead == c) {
+				return;
+			}
 		}
-		
-	}	
+		n.children.add(new Node(c));
+
+	}
 
 }
--- Trie/Trie_Util.java
+++ Trie/Trie_Util.java
@@ -3,6 +3,19 @@ public class Trie_Util {
 	public static void main(String[] args) {
 		ListTrie lt = new ListTrie();
 		lt.insertString("test");
-		
+		lt.insertString("alex");
+		lt.insertString("kelsey");
+		lt.insertString("eclipse");
+		lt.insertString("intellij");
+		lt.insertString("ecmascript");
+		lt.insertString("java");
+		lt.insertString("android");
+		lt.insertString("linux");
+		lt.insertString("javascript");
+		lt.insertString("gcc");
+		lt.insertString("csc");
+		lt.insertString("computer");
+		boolean contains = lt.tSearch("alex");
+		System.out.println(contains);
 	}
 }
